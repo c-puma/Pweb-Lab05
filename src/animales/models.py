58cc301de1476a299a1c934324b82d8ab676b9c5
c-puma <cpumal@unsa.edu.pe>
2022-06-03 10:07:19 -0500

    Solucione el error de models.py

--- src/animales/models.py
+++ src/animales/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Persona(models.Model):
+class Animal(models.Model):
     especie = models.TextField()        
     dieta = models.TextField()
     
